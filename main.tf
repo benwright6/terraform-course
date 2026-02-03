7fc6579d70dba28e5278decbc556407fe0d70e0a
benwright6 <benwright6@hotmail.com>
2026-02-03 14:05:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,13 +9,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "<your subscription id here>"
+  subscription_id = "e93d2c21-b5e1-434d-87f0-f1035d963565"
 }
 
 terraform {
   backend "azurerm" {
     resource_group_name  = "RG1"
-    storage_account_name = "<your storage account>"
+    storage_account_name = "jenkinsstateben"
     container_name       = "terraform-state"
     key                  = "terraform.tfstate"
   }
